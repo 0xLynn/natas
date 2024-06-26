c089e7b7dd9f288d98570b7bcdb863b326bdf9c2
0xLynn <valenjesse27@gmail.com>
2024-06-27 01:11:12 +0700

    fix

--- natas11.py
+++ natas11.py
@@ -1,47 +1,52 @@
-# natas11
-
-import requests
 from pwn import log
-import re
+import requests
 import base64
-import json
+import urllib.parse
+from bs4 import BeautifulSoup
+import re
 
 url = 'http://natas11.natas.labs.overthewire.org'
 username = "natas11"
 password = "UJdqkK1pTu6VLt9UHWAgRZz6sVUZ3lEk"
 auth = (username, password)
-data = '{ "showpassword":"no","bgcolor":"#ffffff"}'
+
+current_data = '{"showpassword":"no","bgcolor":"#ffffff"}'
+new_data = '{"showpassword":"yes","bgcolor":"#ffffff"}'
 
 def xor(text, key):
-        return ''.join(chr(ord(text[i]) ^ ord(key[i % len(key)])) for i in range(len(text)))
+    return ''.join(chr(ord(text[i]) ^ ord(key[i % len(key)])) for i in range(len(text)))
+
 def exploit():
-    json_str = json.dumps(data)
-    enc_json = base64.b64encode(json_str.encode()).decode()
-    log.info("plain cookie : %s", enc_json)
-
     session = requests.Session()
-    session = session.get(url, auth=auth)
-    cookies = session.cookies.get('data')
-    log.info('Encoded Cookie : %s', cookies)
+    response = session.get(url, auth=auth)
+    cookie_value = urllib.parse.unquote(session.cookies.get('data'))
+    log.info('Encoded Cookie : %s', cookie_value)
 
-    cookies = cookies.replace('%3D', '=')
-    cipher = base64.b64decode(cookies)
-    log.info(cipher)
-    key = xor()
-    # log.info('Key : %s', key)
+    decoded_cookie = base64.b64decode(cookie_value).decode()
+    log.info('Decoded Cookie : %s', decoded_cookie)
 
-    enc_data = xor(data, key)
-    cookies = base64.b64encode(enc_data.encode()).decode()
-    log.info('New cookie = %s', cookies)
+    key = xor(current_data, decoded_cookie)
+    log.info('Key : %s', key)
 
-    cookies = {
-        'data': cookies
+    encrypted_data = xor(new_data, 'eDWo')
+    new_cookie_value = base64.b64encode(encrypted_data.encode()).decode()
+    log.info('New Encoded Cookie : %s', new_cookie_value)
+
+    headers = {
+        'Cookie': f'data={new_cookie_value}'
     }
-
-    res = requests.get(url, auth=auth, cookies=cookies)
-    # log.info(res.text)
-
+    response = session.get(url, auth=auth, headers=headers)
+    log.info(response.text)
+    soup = BeautifulSoup(response.text, 'html.parser')
+    try:
+        div = soup.find('div', {'id': 'content'})
+        flag = re.findall('.*natas12.*', div.text)
+        log.info(flag)
+    except:
+        log.error("pre not found")
+        exit(1)
+    
 if __name__ == '__main__':
     exploit()
-    
-# 
+
+# natas12:yZdkjAYZRd3R7tq7T5kXMjMJlOIkzDeB
